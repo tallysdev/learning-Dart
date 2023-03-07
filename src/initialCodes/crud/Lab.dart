8dd335939070eb90cf4433192680720050c3a296
Tallys-aureliano <tallysaureliano@outlook.com>
2023-03-06 21:59:45 -0300

    :sparkles:feat: Crud's First Steps

---
+++ src/initialCodes/crud/Lab.dart
@@ -0,0 +1,33 @@
+abstract class Lab {
+  String name;
+  int colaboradores;
+  List<String> projects = [];
+
+  Lab(this.name, this.colaboradores, List<String> projects);
+
+  getName() => this.name;
+
+  getCollaborators() => this.colaboradores;
+
+  getProjects() => this.projects;
+
+  @override
+  String toString() {
+    // TODO: implement toString
+    return 'Lab{name: $name, colaboradores: $colaboradores, projects: $projects}';
+  }
+}
+
+class LabContructor extends Lab {
+  LabContructor(String name, int colaboradores, List<String> project)
+      : super(name, colaboradores, project) {
+    this.projects = projects;
+  }
+}
+
+void main(List<String> args) {
+  var labican = LabContructor(
+      "Labican", 30, ["TI nas escolas", "Pensamento computacional"]);
+  print(labican);
+  labican.getName();
+}
