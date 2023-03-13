bd38485a057cca6bb6620611741853d667b80f5f
Tallys-aureliano <tallysaureliano@outlook.com>
2023-03-13 17:07:32 -0300

    :sparkles:feat: applying corrections requested by the teacher

--- src/initialCodes/feira.dart
+++ src/initialCodes/feira.dart
@@ -1,21 +1,19 @@
 // Definition of the Vendas class
 class Vendas {
-  final String? date; // sale date, optional (?)
-  final List<Itens>? itens; // list of Itens, optional (?)
+  final String date; // sale date, optional (?)
+  final List<Itens> itens; // list of Itens, optional (?)
   final double res = 0; // fixed value of 0, not used elsewhere
-  double get nota {
-    // getter that calculates the total sale
-    final double valor = itens?.fold(0, (prev, item) => prev! + item.total) ??
-        0; // uses the fold method to sum the values of the Itens in the list (if there are Itens in the list), otherwise returns 0
-    return valor; // returns the total sale value
-  }
 
-  Vendas(this.date,
-      this.itens); // constructor that receives the sale date and the list of Itens
+  // calculates the total sale
+  double nota() => itens.fold(0, (prev, item) => prev + item.total());
+
+  Vendas(
+      {required this.date,
+      required this.itens}); // constructor that receives the sale date and the list of Itens
   @override // toString method override
   String toString() {
     // TODO: implement toString
-    return 'Venda, Data da venda: $date,\n Itens: $itens,\n Total da compra: $nota'; // returns a string that represents the Vendas object
+    return 'Venda, Data da venda: $date,\n Itens: $itens,\n Total da compra: ${this.nota()}'; // returns a string that represents the Vendas object
   }
 }
 
@@ -25,8 +23,10 @@ class Produto {
   final String descricao; // product description
   final String validade; // product expiration date
 
-  Produto(this.preco, this.descricao,
-      this.validade); // constructor that receives the price, description, and expiration date of the product
+  Produto(
+      {required this.preco,
+      required this.descricao,
+      required this.validade}); // constructor that receives the price, description, and expiration date of the product
 
   @override
   String toString() {
@@ -41,36 +41,41 @@ class Itens {
   final double quant; // item quantity
   final Produto produto; // product associated with the item
 
-  Itens(this.quant,
-      this.produto); // constructor that receives the item quantity and the associated product
+  Itens(
+      {required this.quant,
+      required this.produto}); // constructor that receives the item quantity and the associated product
 
-  double get total {
-    // getter that calculates the total cost of the item
-    return (quant *
-        produto
-            .preco); // returns the product price multiplied by the item quantity
-  }
+  double total() =>
+      quant *
+      produto
+          .preco; // returns the product price multiplied by the item quantity
+
+  // double total() => this.quant * this.produto.preco;
 
   @override
   String toString() {
     // toString method override
-    return '\n Quantidade: $quant,\n\t Produto: $produto, Total: $total'; // returns a string that represents the Itens object
+    return '\n Quantidade: $quant,\n\t Produto: $produto, Total: ${this.total()}'; // returns a string that represents the Itens object
   }
 }
 
 void main(List<String> args) {
   // Create some Produto objects
-  var feijao = Produto(3, "feijhao macassa", "10/03/2003");
-  var arroz = Produto(3, "arroz vermelho", "10/03/2003");
-  var carne = Produto(3, "carne de porco", "10/03/2003");
+  var feijao =
+      Produto(preco: 3, descricao: "feijhao macassa", validade: "10/03/2003");
+  var arroz =
+      Produto(preco: 3, descricao: "arroz vermelho", validade: "10/03/2003");
+  var carne =
+      Produto(preco: 3, descricao: "carne de porco", validade: "10/03/2003");
 
   // Create some Itens objects with the Produto objects created above
-  var feijoada = Itens(10, feijao);
-  var arrozDeLeite = Itens(10, arroz);
-  var carneDePorco = Itens(10, carne);
+  var feijoada = Itens(quant: 10, produto: feijao);
+  var arrozDeLeite = Itens(quant: 10, produto: arroz);
+  var carneDePorco = Itens(quant: 10, produto: carne);
 
   // Create a Vendas object with the Itens objects created above
-  var janta = Vendas("10/0534", [feijoada, arrozDeLeite, carneDePorco]);
+  var janta =
+      Vendas(date: "10/0534", itens: [feijoada, arrozDeLeite, carneDePorco]);
 
   // Print the Vendas object using its toString method
   print(janta);
