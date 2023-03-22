0d31568d153d02c2df60f5e8a5df2be01afe3f2e
Tallys-aureliano <tallysaureliano@outlook.com>
2023-03-22 18:50:56 -0300

    :sparkles: Knowing Future, async and await

--- src/initialCodes/HelloWord.dart
+++ src/initialCodes/HelloWord.dart
@@ -1,3 +1,17 @@
-void main(List<String> args) {
-  print("Hello Word");
+void main(List<String> args) async {
+  // Future, async and await
+
+  String nome = "tallys";
+  Future<String> cepFuture = getCepByName("Rua JK");
+  late String cep;
+
+  // cepFuture.then((value) => cep = value);
+
+  cep = await cepFuture;
+
+  print(cep);
+}
+
+Future<String> getCepByName(String name) {
+  return Future.value("1234567");
 }
