bc6bb1728c35b96a96d1a1b1f2b0547abfb9ba8d
Tallys-aureliano <tallysaureliano@outlook.com>
2023-03-29 12:32:58 -0300

    :sparkles: Another way to use Enum

--- src/initialCodes/leaEnum.dart
+++ src/initialCodes/leaEnum.dart
@@ -1,34 +1,26 @@
 void main(List<String> args) {
   var pagamaneto = Pagamento();
-  pagamaneto.pagar(TipoPagamento.PIX);
+  pagamaneto.pagar(TipoPagamento.CARTAO);
 }
 
 enum TipoPagamento {
-  PIX,
-  CARTAO,
-  BOLETO;
-}
+  PIX(3, 'pix'),
+  CARTAO(2, 'cartao'),
+  BOLETO(1, 'boleto');
 
-extension ExtensionTipoPagamento on TipoPagamento {
-  String toValue() {
-    Map map = {
-      TipoPagamento.PIX: 'pix',
-      TipoPagamento.CARTAO: 'cartao',
-      TipoPagamento.BOLETO: 'boleto',
-    };
-
-    return map[this];
-  }
+  final int id;
+  final String value;
+  const TipoPagamento(this.id, this.value);
 }
 
 class Pagamento {
   void pagar(TipoPagamento tipoPagamento) {
-    if (tipoPagamento.toValue() == 'pix') {
-      print('pagamento com pix');
-    } else if (tipoPagamento.toValue() == 'boleto') {
-      print('pagamento por boleto');
-    } else if (tipoPagamento.toValue() == 'cartao') {
-      print('pagamento por cartao');
+    if (tipoPagamento.value == 'pix') {
+      print('pagamento com pix id:${tipoPagamento.id}');
+    } else if (tipoPagamento.value == 'boleto') {
+      print('pagamento por boleto id:${tipoPagamento.id}');
+    } else if (tipoPagamento.value == 'cartao') {
+      print('pagamento por cartao id:${tipoPagamento.id}');
     }
   }
 }
