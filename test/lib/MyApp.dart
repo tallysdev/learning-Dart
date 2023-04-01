c7af108386bfe72169402e1015d04daf0bd680cf
Tallys-aureliano <tallysaureliano@outlook.com>
2023-04-01 14:55:03 -0300

    :sparkles: Instantiating APP, together with themes and constants

---
+++ test/lib/MyApp.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter/material.dart';
+import 'package:test/pages/MyHomePage.dart';
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.red).copyWith(secondary: Colors.blue)
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
