41bb888de625f44c5ec56dd3d823b6f5d558b06b
Tallys-aureliano <tallysaureliano@outlook.com>
2023-04-01 14:58:24 -0300

    :sparkles: Contents of APP components

---
+++ test/lib/pages/MyHomePage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Nubank")
+        ),
+        body: const SingleChildScrollView(child: Center(child: Text("Area de cadastro"))),
+    );
+  }
+}
