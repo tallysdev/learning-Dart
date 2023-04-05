e66536f6b8757ff4435ab724cf676b0958a7ab37
Tallys-aureliano <tallysaureliano@outlook.com>
2023-04-05 14:10:25 -0300

    :truck:chore: trocando de pasta

--- lea-flutter/lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:test/MyApp.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
--- src/aula-28-03.dart
+++ src/aula-28-03.dart
@@ -1,23 +1,23 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-void main() {
-  MaterialApp app = MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue
-      ), 
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Hello Word")
-        ),
-        body: Center(
-        child: Column(
-          children: [
-            Text("Exu")            
-          ])
-        ),
-        bottomNavigationBar: Text("bottom")
-      )
-  );
+// void main() {
+//   MaterialApp app = MaterialApp(
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue
+//       ), 
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text("Hello Word")
+//         ),
+//         body: Center(
+//         child: Column(
+//           children: [
+//             Text("Exu")            
+//           ])
+//         ),
+//         bottomNavigationBar: Text("bottom")
+//       )
+//   );
   
-  runApp(app);
-}
+//   runApp(app);
+// }
