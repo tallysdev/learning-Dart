1c4c9de07f99e17074ed4ba5f63f047b771460f4
Tallys-aureliano <tallysaureliano@outlook.com>
2023-04-12 16:59:26 -0300

    :sparkles:feat: Status management for navbar options

--- receita2/lib/main.dart
+++ receita2/lib/main.dart
@@ -1,23 +1,38 @@
 import 'package:flutter/material.dart';
 
 class NewNavBar extends StatelessWidget {
-  NewNavBar();
+  final List<Icon> icons;
+  final int selectedIndex;
 
-  void botaoFoiTocado(int index) {
-    print("Tocaram no botão $index");
-  }
+  NewNavBar({required this.icons, required this.selectedIndex});
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(onTap: botaoFoiTocado, items: const [
-      BottomNavigationBarItem(
-        label: "Cafés",
-        icon: Icon(Icons.coffee_outlined),
+    assert(icons.length >= 2 && icons.length <= 5);
+
+    return BottomNavigationBar(
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      type: BottomNavigationBarType.fixed,
+      selectedItemColor: Colors.amber,
+      currentIndex: selectedIndex,
+      onTap: (int index) {
+        Navigator.of(context).popUntil((route) => route.isFirst);
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(
+            builder: (BuildContext context) => MyApp(
+              selectedIndex: index,
+            ),
+          ),
+        );
+      },
+      items: List.generate(
+        icons.length,
+        (index) => BottomNavigationBarItem(
+          icon: icons[index],
+          label: '',
+        ),
       ),
-      BottomNavigationBarItem(
-          label: "Cervejas", icon: Icon(Icons.local_drink_outlined)),
-      BottomNavigationBarItem(label: "Nações", icon: Icon(Icons.flag_outlined))
-    ]);
+    );
   }
 }
 
@@ -53,27 +68,55 @@ class NewAppBar extends StatelessWidget implements PreferredSizeWidget {
   }
 
   @override
-  // TODO: implement preferredSize
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
 
-void main() {
-  MyApp app = MyApp();
-  runApp(app);
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key, required this.selectedIndex});
+  final int selectedIndex;
 
   @override
-  Widget build(BuildContext context) {
-    MaterialApp app = MaterialApp(
-        theme: ThemeData.dark(),
-        home: Scaffold(
-          appBar: NewAppBar(),
-          body: NewBody(),
-          bottomNavigationBar: NewNavBar(),
-        ));
-    return app;
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _selectedIndex = 0;
+
+  void onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedIndex = widget.selectedIndex;
+  }
+
+  Widget build(BuildContext context) {
+    final List<Icon> icons = [
+      const Icon(Icons.coffee_outlined),
+      const Icon(Icons.local_drink_outlined),
+      const Icon(Icons.flag_outlined),
+      const Icon(Icons.fastfood_outlined),
+    ];
+
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: Scaffold(
+        appBar: NewAppBar(),
+        body: NewBody(),
+        bottomNavigationBar: NewNavBar(
+          icons: icons,
+          selectedIndex: _selectedIndex,
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  MyApp app = const MyApp(selectedIndex: 0);
+  runApp(app);
 }
