a15da29338ebd7eb5d998e2e42a417f65c8ba174
Tallys-aureliano <tallysaureliano@outlook.com>
2023-07-02 16:39:43 -0300

    :recycle: refactor: adding conditional to meaning

--- receita10/lib/util/ordenador.dart
+++ receita10/lib/util/ordenador.dart
@@ -4,25 +4,20 @@ class Ordenador {
     bool trocouAuMenosUm;
     do {
       trocouAuMenosUm = false;
-      if (!sentido) {
-        // print(itensOrdenados.length);
-        for (int i = 0; i < itensOrdenados.length - 1; i++) {
-          var prox = itensOrdenados[i];
-          var atual = itensOrdenados[i + 1];
+
+      for (int i = 0; i < itensOrdenados.length - 1; i++) {
+        var atual = itensOrdenados[i];
+        var prox = itensOrdenados[i + 1];
+
+        if (sentido) {
           if (atual[propriedade].compareTo(prox[propriedade]) > 0) {
             var aux = itensOrdenados[i];
             itensOrdenados[i] = itensOrdenados[i + 1];
             itensOrdenados[i + 1] = aux;
             trocouAuMenosUm = true;
           }
-        }
-      } else {
-        // print(itensOrdenados.length);
-        for (int i = 0; i < itensOrdenados.length - 1; i++) {
-          // print(i);
-          var atual = itensOrdenados[i];
-          var prox = itensOrdenados[i + 1];
-          if (atual[propriedade].compareTo(prox[propriedade]) > 0) {
+        } else {
+          if (prox[propriedade].compareTo(atual[propriedade]) > 0) {
             var aux = itensOrdenados[i];
             itensOrdenados[i] = itensOrdenados[i + 1];
             itensOrdenados[i + 1] = aux;
